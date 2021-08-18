c04a753c0a6d4193dc1ba1e1a9652215e7f2917f
moise10r <moiserushanika2006@gmail.com>
2021-08-18 15:04:58 +0200

    Refactor TaskList component to functional base component

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,73 +1,67 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { IconContext } from 'react-icons';
 import { AiOutlineDelete } from 'react-icons/ai';
 import { TiEdit } from 'react-icons/ti';
 import { FiMoreHorizontal } from 'react-icons/fi';
 import propTypes from 'prop-types';
 
-class TaskList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      disabled: true,
-      currentTask: null,
-    };
-  }
+const TaskList = ({ onUpdate, onDelete, onComplete }) => {
+  const [state, setstate] = useState({
+    disabled: true,
+    currentTask: null,
+  });
 
-  handleEdit = () => {
-    const { disabled } = this.state;
+  const handleEdit = () => {
+    const { disabled } = state;
     if (!disabled) {
-      this.setState({ disabled: true });
+      setstate({ ...state, disabled: true });
     } else {
-      this.setState({ disabled: false });
+      setstate({ ...state, disabled: false });
     }
-  }
+  };
 
-  handeleShowControlBtn = (id) => {
-    this.setState({ currentTask: id });
-  }
+  const handeleShowControlBtn = (id) => {
+    setstate({ ...state, currentTask: id });
+  };
 
-  render() {
-    const { disabled, currentTask } = this.state;
-    const todos = JSON.parse(localStorage.getItem('tasks')) || [];
-    const { onUpdate, onDelete, onComplete } = this.props;
-    return (
-      <div className="main-tasks-container">
-        <div className="counter-header flex-between">
-          <p>Task table</p>
-          <span className="more">
-            <IconContext.Provider value={{ className: 'icon' }}>
-              <FiMoreHorizontal />
-            </IconContext.Provider>
-          </span>
-        </div>
-        <ul className="task-list">
-          { todos.map(({ id, title, completed }) => (
-            <li key={id} onClick={() => this.handeleShowControlBtn(id)} className="task flex-between">
-              <input type="checkbox" onChange={() => onComplete(id)} name="task" id="check" checked={completed} />
-              <input type="text" onChange={(e) => onUpdate(e, id)} className={completed ? 'completed' : ''} value={title} name="editTask" id="task" disabled={disabled} />
-              <span className={`icons ${currentTask === id ? 'show' : ''}`} id={title}>
-                <span role="button" onClick={this.handleEdit}>
-                  <IconContext.Provider value={{ className: 'icon' }}>
-                    <TiEdit />
-                  </IconContext.Provider>
-                </span>
-                <span role="button" onClick={() => onDelete(id)}>
-                  <IconContext.Provider value={{ className: 'icon' }}>
-                    <AiOutlineDelete />
-                  </IconContext.Provider>
-                </span>
-              </span>
-            </li>
-          ))}
-        </ul>
+  const { disabled, currentTask } = state;
+  const todos = JSON.parse(localStorage.getItem('tasks')) || [];
+  return (
+    <div className="main-tasks-container">
+      <div className="counter-header flex-between">
+        <p>Task table</p>
+        <span className="more">
+          <IconContext.Provider value={{ className: 'icon' }}>
+            <FiMoreHorizontal />
+          </IconContext.Provider>
+        </span>
       </div>
-    );
-  }
-}
+      <ul className="task-list">
+        { todos.map(({ id, title, completed }) => (
+          <li key={id} onClick={() => handeleShowControlBtn(id)} className="task flex-between">
+            <input type="checkbox" onChange={() => onComplete(id)} name="task" id="check" checked={completed} />
+            <input type="text" onChange={(e) => onUpdate(e, id)} className={completed ? 'completed' : ''} value={title} name="editTask" id="task" disabled={disabled} />
+            <span className={`icons ${currentTask === id ? 'show' : ''}`} id={title}>
+              <span role="button" onClick={handleEdit}>
+                <IconContext.Provider value={{ className: 'icon' }}>
+                  <TiEdit />
+                </IconContext.Provider>
+              </span>
+              <span role="button" onClick={() => onDelete(id)}>
+                <IconContext.Provider value={{ className: 'icon' }}>
+                  <AiOutlineDelete />
+                </IconContext.Provider>
+              </span>
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 TaskList.propTypes = {
   onUpdate: propTypes.func.isRequired,
