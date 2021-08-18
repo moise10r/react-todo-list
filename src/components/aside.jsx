8bef650e41e9e31a51619926378ab09a78755079
moise10r <moiserushanika2006@gmail.com>
2021-08-18 14:33:53 +0200

    Refactor aside component to functional base component

--- src/components/aside.jsx
+++ src/components/aside.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 import { IconContext } from 'react-icons';
@@ -9,68 +9,59 @@ import { FaProjectDiagram, FaCalendarAlt } from 'react-icons/fa';
 import { ImMail2 } from 'react-icons/im';
 import logo from '../assets/image/logo.png';
 
-class Aside extends Component {
-  constructor() {
-    super();
-    this.state = {
-      current: null,
-    };
-    this.links = [
-      {
-        id: 1, to: '/', name: 'DashBoard', icon: <AiTwotoneHome />,
-      },
-      {
-        id: 2, to: '', name: 'Project', icon: <FaProjectDiagram />,
-      },
-      {
-        id: 3, to: '', name: 'Calendar', icon: <FaCalendarAlt />,
-      },
-      {
-        id: 4, to: '/contact', name: 'Contact', icon: <ImMail2 />,
-      },
-    ];
-  }
+const Aside = ({ isOpened, onOpenSideBar }) => {
+  const [current, setcurrent] = useState(null);
+  const links = [
+    {
+      id: 1, to: '/', name: 'DashBoard', icon: <AiTwotoneHome />,
+    },
+    {
+      id: 2, to: '', name: 'Project', icon: <FaProjectDiagram />,
+    },
+    {
+      id: 3, to: '', name: 'Calendar', icon: <FaCalendarAlt />,
+    },
+    {
+      id: 4, to: '/contact', name: 'Contact', icon: <ImMail2 />,
+    },
+  ];
 
-  handleActiveLink = (id) => {
-    this.setState({ current: id });
-  }
+  const handleActiveLink = (id) => {
+    setcurrent(id);
+  };
 
-  render() {
-    const { current } = this.state;
-    const { isOpened, onOpenSideBar } = this.props;
-    return (
-      <div className={isOpened ? 'aside-main-wrapper open' : 'aside-main-wrapper'}>
-        <div className="main-logo-wrapper flex-center">
-          <div className="logo-wrapper">
-            <img src={logo} alt="logo" />
-          </div>
-          <span>
-            Tasks Book
-          </span>
+  return (
+    <div className={isOpened ? 'aside-main-wrapper open' : 'aside-main-wrapper'}>
+      <div className="main-logo-wrapper flex-center">
+        <div className="logo-wrapper">
+          <img src={logo} alt="logo" />
         </div>
-        <ul className="link-list flex-start">
-          {this.links.map(({
-            id, name, icon, to,
-          }) => (
-            <li key={id} className="link " role="button" onClick={() => this.handleActiveLink(id)}>
-              <Link to={to} className={`${current === id ? 'active' : ''}`}>
-                <IconContext.Provider value={{ className: 'icon' }}>
-                  {icon}
-                </IconContext.Provider>
-                <span>{name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <button type="button" className="menu-icon flex-center" onClick={onOpenSideBar}>
-          <IconContext.Provider value={{ className: 'icon' }}>
-            <AiOutlineMenuFold />
-          </IconContext.Provider>
-        </button>
+        <span>
+          Tasks Book
+        </span>
       </div>
-    );
-  }
-}
+      <ul className="link-list flex-start">
+        {links.map(({
+          id, name, icon, to,
+        }) => (
+          <li key={id} className="link " role="button" onClick={() => handleActiveLink(id)}>
+            <Link to={to} className={`${current === id ? 'active' : ''}`}>
+              <IconContext.Provider value={{ className: 'icon' }}>
+                {icon}
+              </IconContext.Provider>
+              <span>{name}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <button type="button" className="menu-icon flex-center" onClick={onOpenSideBar}>
+        <IconContext.Provider value={{ className: 'icon' }}>
+          <AiOutlineMenuFold />
+        </IconContext.Provider>
+      </button>
+    </div>
+  );
+};
 
 Aside.propTypes = {
   isOpened: propTypes.bool.isRequired,
