bca6df50aa2171a98232a5b1385b2875d3e6fb05
moise10r <moiserushanika2006@gmail.com>
2021-08-18 14:59:32 +0200

    Refactor message component to functional base component

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,20 +1,9 @@
 /* eslint-disable max-len */
-import React, { Component } from 'react';
-
-class Message extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="message-wrapper">
-        <span className="name">Moise</span>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere debitis cum, veniam laboriosam 🙂😛</p>
-      </div>
-    );
-  }
-}
+const Message = () => (
+  <div className="message-wrapper">
+    <span className="name">Moise</span>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere debitis cum, veniam laboriosam 🙂😛</p>
+  </div>
+);
 
 export default Message;
