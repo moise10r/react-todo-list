6a1126b4ea09b49a59cd497d2fadad969e7aefdf
moise10r <moiserushanika2006@gmail.com>
2021-08-18 14:43:51 +0200

    Refactor header component to functional base component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,45 +1,34 @@
-import { Component } from 'react';
 import { IconContext } from 'react-icons';
 import { AiOutlinePlusCircle } from 'react-icons/ai';
 import { RiMoonClearFill } from 'react-icons/ri';
 import propTypes from 'prop-types';
 import profile from '../assets/image/profile.png';
 
-class Header extends Component {
-  constructor() {
-    super();
-    this.state = { };
-  }
-
-  render() {
-    const { onClick } = this.props;
-    return (
-      <div className="main-header-wrapper">
-        <div className="header-content-wrapper flex-between">
-          <button onClick={onClick} type="button" className="add-task flex-center">
-            {' '}
-            <IconContext.Provider value={{ className: 'icon' }}>
-              <AiOutlinePlusCircle />
-            </IconContext.Provider>
-            {' '}
-            <span>Add New Task</span>
-          </button>
-          <button type="button" className="change-mode">
-            <IconContext.Provider value={{ className: 'icon' }}>
-              <RiMoonClearFill />
-            </IconContext.Provider>
-          </button>
-          <div className="auth flex-center">
-            <span>Moise Rushanika</span>
-            <div className="profile-img">
-              <img src={profile} alt="profile" />
-            </div>
-          </div>
+const Header = ({ onClick }) => (
+  <div className="main-header-wrapper">
+    <div className="header-content-wrapper flex-between">
+      <button onClick={onClick} type="button" className="add-task flex-center">
+        {' '}
+        <IconContext.Provider value={{ className: 'icon' }}>
+          <AiOutlinePlusCircle />
+        </IconContext.Provider>
+        {' '}
+        <span>Add New Task</span>
+      </button>
+      <button type="button" className="change-mode">
+        <IconContext.Provider value={{ className: 'icon' }}>
+          <RiMoonClearFill />
+        </IconContext.Provider>
+      </button>
+      <div className="auth flex-center">
+        <span>Moise Rushanika</span>
+        <div className="profile-img">
+          <img src={profile} alt="profile" />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 Header.propTypes = {
   onClick: propTypes.func.isRequired,
